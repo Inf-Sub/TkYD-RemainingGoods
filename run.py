92f39acf5f09bc4bcda9c1d85e55bd38a1f03072
InfSub <inf.sub+github.com@gmail.com>
2024-12-19 04:09:08 +0300

    version 2.7.6.1

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 __author__ = 'InfSub'
 __contact__ = 'ADmin@TkYD.ru'
 __copyright__ = 'Copyright (C) 2024, [LegioNTeaM] InfSub'
-__date__ = '2024/12/18'
+__date__ = '2024/12/19'
 __deprecated__ = False
 __email__ = 'ADmin@TkYD.ru'
 __maintainer__ = 'InfSub'
 __status__ = 'Development'  # 'Production / Development'
-__version__ = '2.7.5'
+__version__ = '2.7.6'
 
 from asyncio import run as aio_run, sleep as aio_sleep, create_task as aio_create_task, gather as aio_gather
 # from asyncio import get_event_loop as aio_get_event_loop
@@ -63,31 +63,14 @@ async def perform_smb_task(srv_status, shop_id: str) -> None:
         logger.debug(f'Время подключения к "{hostname}": {time() - start_connect_time:.2f} секунд.')
 
         if file_path:
-            # convert_encoding перенесен в класс CSVHandler
-            # try:
-            #     await convert_encoding(file_path)  # Конвертируем файл в UTF-8, если необходимо
-            # except Exception as e:
-            #     logger.error(f'Ошибка при конвертации файла магазина {shop_id} в UTF-8: {e}')
 
             try:
-                # handler = CSVHandler()
                 handler = CSVValidator()
                 valid_records = await handler.process_csv(file_path)
 
                 # await run_csv_reader(file_path)
             except Exception as e:
                 if not debug_mode:
-                    # Получаем текущее имя функции и номер строки
-                    # current_frame = inspect.currentframe()
-                    # Получаем исходный код текущей и предыдущей инструкций
-                    # try:
-                    #     function_name = inspect.getframeinfo(current_frame.f_back).function
-                    #     line_number = inspect.getframeinfo(current_frame.f_back).lineno
-                    # finally:
-                    #     # Удаляем ссылку на фрейм, чтобы избежать утечек памяти
-                    #     del current_frame
-                    # logger.error(
-                    #     f'Ошибка в функции {function_name} на строке {line_number} при чтении файла {shop_id} в UTF-8: {e}')
                     logger.error(f'Ошибка при чтении файла магазина {shop_id} в UTF-8: {e}')
                 else:
                     raise
@@ -97,21 +80,12 @@ async def perform_smb_task(srv_status, shop_id: str) -> None:
                 if valid_records:
                     logger.info('Validation successful.')
                     # импортируем данные в БД
-                    # pprint([shop_id, valid_records])
                     await update_data(shop_id, valid_records)
                 
                 else:
                     logger.warning('Failed to process records.')
             except Exception as e:
                 if not debug_mode:
-                    # current_frame = inspect.currentframe()
-                    # try:
-                    #     function_name = inspect.getframeinfo(current_frame.f_back).function
-                    #     line_number = inspect.getframeinfo(current_frame.f_back).lineno
-                    # finally:
-                    #     del current_frame
-                    # logger.error(
-                    #     f'Ошибка в функции {function_name} на строке {line_number} при импорте данных в БД {shop_id}: {e}')
                     logger.error(f'Ошибка при импорте данных в БД для магазина {shop_id}: {e}')
                 else:
                     raise
@@ -125,7 +99,6 @@ async def perform_smb_task(srv_status, shop_id: str) -> None:
             raise
     finally:
         # Обновление статуса сервера
-        # await update_server_status(hostname, bool(file_path))
         await srv_status.set_value(hostname, bool(file_path))
 
         logger.info(f'Обновление статуса сервера: "{hostname}"')
